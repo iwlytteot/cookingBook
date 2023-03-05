5330e3d193e8f3665ccd09ac41c8cc413199d8fb
Leo Tovaryš <leosekt@gmail.com>
2023-03-05 21:04:41 +0100

    Removing FE

--- cookingBook-frontend/src/app/model/recipe.ts
+++
@@ -1,9 +0,0 @@
-export interface Recipe {
-  id: number;
-  name: string;
-  description: string;
-  portion: number;
-  timeComplexity: { baseEstimate: string; additionalEstimate: string; }
-  createTime: string;
-  imageId: string;
-}
