37af9cf7bbd7c99f07c5ce9a57c1575d8767d4a0
Leo Tovaryš <leosekt@gmail.com>
2023-04-16 14:02:02 +0200

    Removing unused test

--- src/test/java/com/iwlytteot/cookingBook/CookingBookApplicationTests.java
+++ src/test/java/com/iwlytteot/cookingBook/CookingBookApplicationTests.java
@@ -1,17 +1,8 @@
 package com.iwlytteot.cookingBook;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.transaction.Transactional;
 
 @SpringBootTest
 class CookingBookApplicationTests {
-
-
-	@Test
-	@Transactional
-	void contextLoads() {
-	}
 
 }
